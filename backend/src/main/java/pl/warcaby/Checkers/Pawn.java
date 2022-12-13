3bbfaab1a0a82248f26eaa6ad3678a23bdf15f88
sebawoz02 <268491@student.pwr.edu.pl>
2022-12-13 17:53:00 +0100

    Dodano konstruktor klasy Pawn oraz utworzono jej metody.

--- backend/src/main/java/pl/warcaby/Checkers/Pawn.java
+++ backend/src/main/java/pl/warcaby/Checkers/Pawn.java
@@ -5,4 +5,21 @@ public class Pawn {
     Player owner;
 
     PawnType type;
+
+    public Pawn(Player owner){
+        this.owner = owner;
+        this.type = PawnType.NORMAL;
+    }
+
+    public Color getColor(){
+        return this.owner.getColor();
+    }
+
+    public PawnType getType() {
+        return type;
+    }
+
+    public void setType(PawnType type){
+        this.type = type;
+    }
 }
