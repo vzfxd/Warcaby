72c75aaa57e4658a5f2e094106fcb5a841fb0d84
sebawoz02 <268491@student.pwr.edu.pl>
2022-12-13 18:41:12 +0100

    Dodano abstrakcyjne metody w klasie Board

--- backend/src/main/java/pl/warcaby/Checkers/Board.java
+++ backend/src/main/java/pl/warcaby/Checkers/Board.java
@@ -1,8 +1,15 @@
 package pl.warcaby.Checkers;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public abstract class Board {
 
     Field[][] fields;
 
     Color turn;
+
+    public abstract ArrayList<int[]> checkBestPawnMoves();
+
+    public abstract ArrayList<int[][]> checkBestMoves();
+
 }
