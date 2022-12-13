c4a1fd7d0f93325d1a5d5c84c7801ec5a6e73528
sebawoz02 <268491@student.pwr.edu.pl>
2022-12-13 19:12:06 +0100

    Zmieniono metode checkBestMoves w Board i utworzono klase SpanishBoard

---
+++ backend/src/main/java/pl/warcaby/Checkers/Variations/SpanishBoard.java
@@ -0,0 +1,14 @@
+package pl.warcaby.Checkers.Variations;
+
+import pl.warcaby.Checkers.Board;
+import pl.warcaby.Checkers.Color;
+
+import java.util.ArrayList;
+
+public class SpanishBoard extends Board {
+    @Override
+    public ArrayList<int[]> checkBestPawnMoves(int[] pawnLocation) {
+        return null;
+    }
+
+}
