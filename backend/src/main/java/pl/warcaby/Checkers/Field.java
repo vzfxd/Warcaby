454cfb2bff89bfc60034c626b7c46898c988c81e
sebawoz02 <268491@student.pwr.edu.pl>
2022-12-13 18:02:44 +0100

    Dodano dwa rozne konstrukotry w klasie Field oraz metody dotyczace atrybutu Field.occupied

--- backend/src/main/java/pl/warcaby/Checkers/Field.java
+++ backend/src/main/java/pl/warcaby/Checkers/Field.java
@@ -1,5 +1,25 @@
 package pl.warcaby.Checkers;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Field {
-    Pawn occupied;
+    Pawn occupied = null;
+
+    public Color getPawnColor() {
+        if (occupied != null) {
+            return this.occupied.getColor();
+        }
+        return null;
+    }
+
+    public void pawnTypeUpgrade(){
+        this.occupied.setType(PawnType.QUEEN);
+    }
+
+    public void setOccupied(Pawn pawn){
+        this.occupied = pawn;
+    }
 }
