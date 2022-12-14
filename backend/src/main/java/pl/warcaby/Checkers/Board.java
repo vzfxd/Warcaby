d32f0ad42e9f5474a51fbdd89f350ee8520f6f06
sebawoz02 <268491@student.pwr.edu.pl>
2022-12-14 13:04:34 +0100

    Javadoc comments added.

--- backend/src/main/java/pl/warcaby/Checkers/Board.java
+++ backend/src/main/java/pl/warcaby/Checkers/Board.java
@@ -2,19 +2,31 @@ package pl.warcaby.Checkers;
 import java.util.ArrayList;
 
 
+/**Abstrakcyjna klasa planszy na której toczy się rozgrywka gry w warcaby.*/
 public abstract class Board {
-
+    /**Pola planszy, dla każdej odmiany gry rozmiar tej tablicy może być rózny.*/
     protected Field[][] fields;
-
+    /**Kolor gracza który aktualnie może wykonać ruch.*/
     protected Color turn;
-
+    /**Szerokosc tablicy.*/
     protected int width;
-
+    /**Wysokosc tablicy.*/
     protected int heigth;
-
+    /**Abstrakcyjna metoda wyświetlająca najlepsze ruchy dla pionka na podanej lokalizacji.
+     *
+     * @param pawnLocation lokalizacja pionka na planszy.
+     * @return Lista dostępnych ruchów dla pionka.*/
     public abstract ArrayList<int[]> checkBestPawnMoves(int[] pawnLocation);
+    /**Abstrakcyjna metoda znajdująca wszystkie możliwe ruchy dla pionka na podanej lokazlizacji.
+     *
+     * @param pawnLocation lokalizacja pionka na planszy.
+     * @return Listy z ruchami dostępnymi dla pionka.*/
     protected abstract ArrayList<ArrayList<int[]>> getAllMoves(int[] pawnLocation);
 
+    /**Metoda szukająca możliwych bić na planszy dla gracza z danym kolorem.
+     *
+     * @param color kolor gracza, dla którego bic szukamy.
+     * @return Listy dostępnych najlepszych bić dla gracza. W przypadku gdy nie ma zadnych bic zwraca null.*/
     public ArrayList<ArrayList<int[]>> checkBestMoves(Color color){
         ArrayList<ArrayList<int[]>> bestMoves = new ArrayList<>();
         int bestMoveLength = 0;
@@ -41,7 +53,7 @@ public abstract class Board {
         }
         return bestMoves;
     }
-
+    /**Metoda zmieniajaca kolejke na drugiego gracza.*/
     public void changeTurn(){
         if(this.turn == Color.BLACK){
             this.turn = Color.WHITE;
@@ -50,6 +62,9 @@ public abstract class Board {
             this.turn = Color.BLACK;
         }
     }
+    /**Abstrakcyjna metoda wykonywania ruchu na planszy.
+     *
+     * @param steps Lista zawierająca opis ruchu.*/
     public abstract void move(ArrayList<int[]> steps);
 
 }
