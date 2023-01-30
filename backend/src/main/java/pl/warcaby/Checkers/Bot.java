c8f0c6c42be8a76f37ed2eba8a5648f03438424a
sebawoz02 <268491@student.pwr.edu.pl>
2023-01-30 08:17:09 +0100

    .

--- backend/src/main/java/pl/warcaby/Checkers/Bot.java
+++ backend/src/main/java/pl/warcaby/Checkers/Bot.java
@@ -14,8 +14,9 @@ public class Bot extends Player{
             ArrayList<ArrayList<int[]>> moves = board.checkBestMoves(this.color);
             ArrayList<int[]> botmove = new ArrayList<>();
             int n = rand.nextInt(moves.size());
+            int m = rand.nextInt(moves.get(n).size()-2);
             botmove.add(moves.get(n).get(1));
-            botmove.add(moves.get(n).get(2));
+            botmove.add(moves.get(n).get(m+2));
             return botmove;
     }
 }
