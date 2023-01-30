e67a424481854edfb6f1fcd24c0034551f122240
sebawoz02 <268491@student.pwr.edu.pl>
2023-01-30 08:27:58 +0100

    .

--- backend/src/main/java/pl/warcaby/Checkers/Bot.java
+++ backend/src/main/java/pl/warcaby/Checkers/Bot.java
@@ -14,9 +14,17 @@ public class Bot extends Player{
             ArrayList<ArrayList<int[]>> moves = board.checkBestMoves(this.color);
             ArrayList<int[]> botmove = new ArrayList<>();
             int n = rand.nextInt(moves.size());
-            int m = rand.nextInt(moves.get(n).size()-2);
+            int m;
             botmove.add(moves.get(n).get(1));
-            botmove.add(moves.get(n).get(m+2));
+            if (moves.get(n).get(0)[0] > 0) {
+                m = rand.nextInt((moves.get(n).size() - 2) / moves.get(n).get(0)[0]);
+                botmove.add(moves.get(n).get(m*moves.get(n).get(0)[0] + 2));
+            }
+            else{
+                m = rand.nextInt(moves.get(n).size() - 2);
+                botmove.add(moves.get(n).get(m + 2));
+            }
+
             return botmove;
     }
 }
